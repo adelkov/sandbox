98a6e9c29dc3f859b5e2b0f6a08d3fdb8d39e3be
Adel Kovacs <kovacsadel12@gmail.com>
2019-01-29 15:45:48 +0100

    feature: add new user

--- accounts/src/main/java/com/hc/accounts/Application.java
+++ accounts/src/main/java/com/hc/accounts/Application.java
@@ -1,11 +1,38 @@
 package com.hc.accounts;
 
+import com.hc.accounts.core.AccountsService;
+import com.hc.accounts.core.data.UserDTOMemory;
+import com.hc.accounts.core.data.UserModel;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.json.JsonObject;
+
+import java.math.BigDecimal;
 
 public class Application extends AbstractVerticle {
+    private EventBus eventBus;
+
     @Override
     public void start() throws Exception {
         super.start();
-        System.out.println("accounts service starts...");
+        vertx.deployVerticle(new AccountsService(new UserDTOMemory()));
+        this.eventBus = vertx.eventBus();
+        addUserTest();
+    }
+
+    private void addUserTest() {
+        JsonObject user = new JsonObject()
+                .put("userName", "Alma Fa")
+                .put("userId", "Id131313")
+                .put("balance", 0);
+        this.eventBus.send("ADD_USER", user, res -> {
+            res.map(result -> {
+                System.out.println(result.body());
+                return result;
+            }).otherwise(error -> {
+                System.out.println(error.getMessage());
+                return null;
+            });
+        });
     }
 }
--- accounts/src/main/java/com/hc/accounts/core/AccountsService.java
+++ accounts/src/main/java/com/hc/accounts/core/AccountsService.java
@@ -1,4 +1,43 @@
 package com.hc.accounts.core;
 
-public class AccountsService {
+import com.hc.accounts.core.data.UserDTOMemory;
+import com.hc.accounts.core.data.UserModel;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.json.JsonObject;
+
+public class AccountsService extends AbstractVerticle {
+
+    private UserDTOMemory userDTOMemory;
+
+    public AccountsService(UserDTOMemory userDTOMemory) {
+        this.userDTOMemory = userDTOMemory;
+    }
+
+    @Override
+    public void start() throws Exception {
+        super.start();
+        vertx.eventBus().consumer("ADD_USER", this::addUser);
+        vertx.eventBus().consumer("DEPOSIT", this::deposit);
+        vertx.eventBus().consumer("WITHDRAW", this::withdraw);
+    }
+
+    private void addUser(final Message<JsonObject> message) {
+        UserModel user = new UserModel(
+                message.body().getString("userName"),
+                message.body().getString("userId"),
+                message.body().getInteger("balance")
+        );
+        userDTOMemory.addUser(user);
+
+        message.reply(JsonObject.mapFrom(user));
+    }
+
+    private void deposit(Message message) {
+
+    }
+
+    private void withdraw(Message message) {
+
+    }
 }
