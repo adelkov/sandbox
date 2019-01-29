ea54e1cd8d265d0ae85fef5e5dab053839c2dcea
Adel Kovacs <kovacsadel12@gmail.com>
2019-01-29 13:38:04 +0100

    project build

--- hello-vertx/src/main/java/com/hc/hellovertx/Application.java
+++ hello-vertx/src/main/java/com/hc/hellovertx/Application.java
@@ -1,9 +1,15 @@
 package com.hc.hellovertx;
 
 import com.hc.addition.api.AdditionEventbusClient;
+import com.hc.addition.api.AdditionRestClient;
 import com.hc.addition.core.AdditionService;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.WebClientOptions;
+
+import java.util.function.Function;
 
 
 public class Application extends AbstractVerticle {
@@ -11,14 +17,31 @@ public class Application extends AbstractVerticle {
     @Override
     public void start() throws Exception {
         super.start();
+        JsonObject addends = new JsonObject().put("x", "12").put("y", "111");
 
+        // Event bus client
         vertx.deployVerticle(new AdditionService());
         AdditionEventbusClient additionEventbusClient = new AdditionEventbusClient(vertx.eventBus());
-
-        JsonObject object = new JsonObject().put("x", "12").put("y", "111");
-        additionEventbusClient.add(object).setHandler(res -> {
-            System.out.println(res.result());
+        additionEventbusClient.add(addends).map(res -> {
+            System.out.println("Result with EventBus client: " + res);
+            return res;
         });
-        System.out.println("Hello Vert.x!");
+
+        // Rest client
+        WebClientOptions options = new WebClientOptions();
+        options
+                .setDefaultHost("localhost")
+                .setDefaultPort(8080)
+                .addCrlPath("/add");
+        WebClient webClient = WebClient.create(vertx, options);
+        AdditionRestClient additionRestClient = new AdditionRestClient(webClient);
+        additionRestClient.add(addends)
+                .map(res -> {
+                    System.out.println("Result with rest client: " + res);
+                    return res;
+                }).otherwise(error -> {
+            System.out.println(error.getMessage());
+            return null;
+        });
     }
 }
